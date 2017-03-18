c4c4ee518ca1fe2d9aa1bcceb4282efcea38b2af
richelbilderbeek <richel@richelbilderbeek.nl>
2017-03-18 20:20:22 +0100

    Compiles and runs locally

--- ndsmake.cpp
+++ ndsmake.cpp
@@ -1,23 +1,3 @@
-//---------------------------------------------------------------------------
-/*
-ndsmake, tool to generate NDS makefile from Qt Creator project file
-Copyright (C) 2010-2015 Richel Bilderbeek
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.If not, see <http://www.gnu.org/licenses/>.
-*/
-//---------------------------------------------------------------------------
-// From http://www.richelbilderbeek.nl/ToolNdsmake.htm
-//---------------------------------------------------------------------------
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Weffc++"
 #include "ndsmake.h"
@@ -39,9 +19,6 @@ ribi::ndsm::Ndsmake::Ndsmake(
     m_command{},
     m_pro_file(pro_file_name)
 {
-  #ifndef NDEBUG
-  Test();
-  #endif
   if (!fileio::FileIo().IsRegularFile(pro_file_name))
   {
     std::stringstream msg;
@@ -180,19 +157,3 @@ std::string ribi::ndsm::Ndsmake::RemoveExtension(const std::string& filename)
   assert(dot_index != -1 && "No dot found in filename");
   return filename.substr(0,dot_index);
 }
-
-#ifndef NDEBUG
-void ribi::ndsm::Ndsmake::Test() noexcept
-{
-  ///Test exactly once
-  {
-    static bool is_tested{false};
-    if (is_tested) return;
-    is_tested = true;
-  }
-  {
-    Ndsmake a("/home/p230198/GitHubs/ndsmake/ndsmake.pro","../ndsmake/ndsmake.pro");
-    Ndsmake b(a);
-  }
-}
-#endif
--- ndsmake.h
+++ ndsmake.h
@@ -1,23 +1,3 @@
-//---------------------------------------------------------------------------
-/*
-ndsmake, tool to generate NDS makefile from Qt Creator project file
-Copyright (C) 2010-2015 Richel Bilderbeek
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.If not, see <http://www.gnu.org/licenses/>.
-*/
-//---------------------------------------------------------------------------
-// From http://www.richelbilderbeek.nl/ToolNdsmake.htm
-//---------------------------------------------------------------------------
 #ifndef NDSMAKE_H
 #define NDSMAKE_H
 
@@ -62,10 +42,6 @@ struct Ndsmake
 
   //From http://www.richelbilderbeek.nl/CppRemoveExtension.htm
   static std::string RemoveExtension(const std::string& filename);
-
-  #ifndef NDEBUG
-  static void Test() noexcept;
-  #endif
 };
 
 } //~namespace ndsm
