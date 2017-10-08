6c259c1af5a7d215fecc638a7f02872484898e31
richelbilderbeek <richel@richelbilderbeek.nl>
2017-10-08 21:09:31 +0200

    Test works locally

--- ndsmake_test.cpp
+++ ndsmake_test.cpp
@@ -6,6 +6,8 @@ BOOST_AUTO_TEST_CASE(ndsmake_needs_valid_input_file)
 {
   const std::string argv0 = "./";
   const std::string pro_file_name = "abs.ent";
-  BOOST_CHECK_THROW(ribi::ndsm::Ndsmake(argv0, pro_file_name), std::invalid_argument);
+  BOOST_CHECK_THROW(
+    ribi::ndsm::Ndsmake(argv0, pro_file_name),
+    std::invalid_argument);
 }
 
