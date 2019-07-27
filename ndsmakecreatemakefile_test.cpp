e16c2402d812e69d24d1e345f4d433afd9757023
richelbilderbeek <richel@richelbilderbeek.nl>
2019-07-27 15:17:21 +0200

    Download test .pro file

--- ndsmakecreatemakefile_test.cpp
+++ ndsmakecreatemakefile_test.cpp
@@ -8,7 +8,7 @@ using namespace ribi::ndsm;
 BOOST_AUTO_TEST_CASE(CreateMakefile_creates_a_makefile)
 {
   ribi::QtCreatorProFile pro_file(
-    "/home/richel/GitHubs/libnds_example_1/libnds_example_1.pro"
+    "libnds_example_1.pro"
   );
   CreateMakefile(pro_file);
 
